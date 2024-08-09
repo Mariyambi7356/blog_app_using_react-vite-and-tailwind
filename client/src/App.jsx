21c6a58b3e4cf47e7258cc49e2d0086dfd7a2656
Mariyambi P <mariyambip5@gmail.com>
2024-08-09 14:26:39 +0530

    create and add footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Project from "./pages/Project";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
    <Header/>
+   
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -18,6 +20,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/" element={<SignUp />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }

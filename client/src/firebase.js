5244630f6823011973f4e0dfb132098ca4c490ce
Mariyambi P <mariyambip5@gmail.com>
2024-08-20 15:05:04 +0530

    add OAuth functionality

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+
+
+const firebaseConfig = {
+    
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-blog-be866.firebaseapp.com",
+  projectId: "mern-blog-be866",
+  storageBucket: "mern-blog-be866.appspot.com",
+  messagingSenderId: "721576287345",
+  appId: "1:721576287345:web:f1e7b828c92e6f5a9acfd0"
+};
+
+
+export const app = initializeApp(firebaseConfig);

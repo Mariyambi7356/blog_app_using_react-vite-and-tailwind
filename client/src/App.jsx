ad34d00e91e216806346c9275e2943ea46e4274a
Mariyambi P <mariyambip5@gmail.com>
2024-08-21 15:01:50 +0530

    created private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,13 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
-   <Header/>
-   
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -19,8 +20,9 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }

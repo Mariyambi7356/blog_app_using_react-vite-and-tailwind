28e01740012ae53fe58b1dc50e61cb1bf2d78f13
Mariyambi P <mariyambip5@gmail.com>
2024-09-17 16:11:58 +0530

    create get post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,10 @@
-import express from 'express'
-import { verifyToken } from '../utils/VerifyUser.js'
-import { create } from '../controller/post.controller.js'
+import express from "express";
+import { verifyToken } from "../utils/VerifyUser.js";
+import { create, getposts } from "../controller/post.controller.js";
 
-const router=express.Router()
+const router = express.Router();
 
+router.post("/create", verifyToken, create);
+router.get("/getposts", getposts);
 
-router.post('/create',verifyToken,create)
-
-
-export default router
+export default router;

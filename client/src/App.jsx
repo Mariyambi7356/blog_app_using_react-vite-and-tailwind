e46fe7e4dc18fb3fcacddfa06c56e02683b945b3
Mariyambi P <mariyambip5@gmail.com>
2024-09-18 15:54:43 +0530

    complete search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
 import ScrollToTop from "./components/ScrollToTop";
-
+import Search from "./pages/Search";
 export default function App() {
   return (
     <BrowserRouter>
@@ -26,6 +26,7 @@ export default function App() {
         <Route path="/project" element={<Project />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />

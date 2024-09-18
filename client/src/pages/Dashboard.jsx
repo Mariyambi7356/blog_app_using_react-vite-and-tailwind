32a85f3dedd1ce9355f780bfbeaeea1e1a108e18
Mariyambi P <mariyambip5@gmail.com>
2024-09-18 15:40:23 +0530

    Add comments section to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import DashSidebar from "../components/DashSideBar";
 import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPost";
 import DashUsers from "../components/DashUsers";
+import DashComments from "../DashComments";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -27,6 +28,8 @@ export default function Dashboard() {
        {tab === "posts" && <DashPosts />}
       {/* users */}
       {tab === "users" && <DashUsers />}
+       {/* comments */}
+       {tab === "comments" && <DashComments />}
     </div>
   );
 }

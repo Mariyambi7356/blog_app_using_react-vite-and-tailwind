b926b4e7adb9b8623dfc7585e0c807cb1b945644
Mariyambi P <mariyambip5@gmail.com>
2024-09-18 14:45:51 +0530

    Show the comments of a post

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
 
-import { verifyToken } from "../utils/VerifyUser.js";
-import { deleteUser, getUsers,  signout, test, updateUser } from "../controller/user.controller.js";
+
+import { deleteUser, getUser, getUsers,  signout, test, updateUser } from "../controller/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
@@ -10,4 +11,5 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post('/signout',signout)
 router.get("/getusers", verifyToken, getUsers);
+router.get('/:userId',getUser)
 export default router;
